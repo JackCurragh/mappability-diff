599305a26d9cbb580634cbf07a76b86051150988
JackCurragh <JackCurragh@gmail.com>
2024-08-27 16:52:02 +0000

    updated run genmap to be universally applicatble

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # mappability-diff
 Compare mappability tracks 
+
+
+
+## Setup and Installation 
+
+```
+conda create -n mappability-diff
+
+conda install bioconda::ucsc-wigtobigwig python=3.10 
+
+python -m pip install -r requirements.txt
+``` 
+
+
+## Run 
+
+Create `data` directory with `fasta` and `bigwig` subdirectories. 
+```
+mkdir -p data/fasta
+mkdir -p data/bigwig 
+```
+Move your fasta files to `data/fasta` before proceding
+
+
+Run genmap to create mappability tracks 
+```
+
+```
+
+
+```
+python src/mappability_diff.py data/bigwig results --gtf annotation.gtf
+```
+
--- run_genmap.sh
+++ run_genmap.sh
@@ -1,16 +1,7 @@
 
-# Generate chromosome sizes file using seqkit
-generate_chrom_sizes() {
-    chrName=$1
-    chrLenght=$2
-    chrom_sizes_file=$3	
-
-    echo "Generating chromosome sizes file: $chrom_sizes_file"
-    paste $chrName $chrLength > $chrom_sizes_file
-}
 
 # Iterate over each fasta file matching *toplevel.fa
-for fasta_file in $(find . -type f -name "*toplevel.fa"); do
+for fasta_file in $(find . -type f -name "data/fasta/*.fa"); do
     # Get the directory of the fasta file
     dir=$(dirname "$fasta_file")
     
@@ -22,29 +13,9 @@ for fasta_file in $(find . -type f -name "*toplevel.fa"); do
     
     # Generate chromosome sizes file
     chrom_sizes_file="${dir}/${base_name}_chrom_sizes.txt"
-    generate_chrom_sizes "${dir}/STAR_index/genomeDir/chrName.txt" "${dir}/STAR_index/genomeDir/chrLenght.txt" "$chrom_sizes_file"
+    samtools faidx $fasta_file
+    cut -f1,2 ${fasta_file}.fai > $chrom_sizes_file
 
-    # if ${dir}/21mers_mappability.wig exists, skip this directory
-    if [ -f "${dir}/21mers_mappability.wig" ]; then
-
-    for wig_file in $(find "$dir/" -type f -name "*.wig"); do
-
-	# Get the base name of the WIG file without the extension
-        wig_base_name=$(basename "$wig_file" .wig)
-        echo $wig_base_name
-        
-    	chrom_sizes_file="${dir}/${wig_base_name}.chrom.sizes"
-	# Create the BigWig file name
-        bw_file="${dir}/${wig_base_name}.bw"
-        
-        # Convert WIG to BigWig
-        wigToBigWig "$wig_file" "$chrom_sizes_file" "$bw_file"
-        
-        echo "Converted $wig_file to $bw_file"
-    done
-    continue
-    fi
-    
     # Create the index for genmap
     genmap index --fasta-file "$fasta_file" --index "$index_file"
     
